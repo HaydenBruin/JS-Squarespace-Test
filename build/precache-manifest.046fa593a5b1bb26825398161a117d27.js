90542d221a024a8b80ee90a24d02fd4544151423
Hayden Bruin <Hayden.Bruin@cucumber.co.nz>
2019-12-04 16:43:59 +1300

    adding built files

---
+++ build/precache-manifest.046fa593a5b1bb26825398161a117d27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d1f7ff7d1035578df394dd0cd954f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f48e9ba83cfdcf72ba72",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c3d2f6adde46756d5af3",
+    "url": "/static/js/2.a0b008bb.chunk.js"
+  },
+  {
+    "revision": "f48e9ba83cfdcf72ba72",
+    "url": "/static/js/main.efe510d1.chunk.js"
+  },
+  {
+    "revision": "0d96d41f4c106e24d6d8",
+    "url": "/static/js/runtime-main.fe454a21.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
